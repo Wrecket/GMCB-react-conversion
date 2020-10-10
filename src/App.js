1b46fdeb4329d589b00e3a78b88902780cbf7e78
Matthew Fuller <matt.j.fuller@outlook.com>
2020-10-10 20:06:06 +0100

    update

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import axios from "./mock/mockAxios";
+import "./styles.css";
+import Scroll from "../src/components/Scroll";
+import CardList from "./components/CardList/CardList";
+
+function App() {
+  const [recipes, setRecipes] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    (async function fetchRecipes() {
+      const { data } = await axios.get("/recipes");
+      setRecipes(data);
+    })();
+  }, []);
+
+  const filteredRecipes = () => {
+    return recipes.filter(recipe => {
+      return recipe.name.toLowerCase().includes(searchTerm.toLowerCase());
+    });
+  };
+
+  return (
+    <div className="wrap f1 tc">
+      <h1 className="f1 tc">Recipes</h1>
+
+      <input className="" onChange={e => setSearchTerm(e.target.value)} value={searchTerm} />
+
+      <div className="">
+        <Scroll>
+        <CardList recipes={filteredRecipes()} />
+        </Scroll>
+      </div>
+    </div>
+  );
+}
+
+export default App;
