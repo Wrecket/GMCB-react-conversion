4652aee0712a499e7950e91acc236687a19ad0b3
Matthew Fuller <matt.j.fuller@outlook.com>
2020-10-15 12:42:33 +0100

    revert

--- src/Component/Welcome/Welcome.js
+++ src/Component/Welcome/Welcome.js
@@ -61,10 +61,16 @@ const Welcome = () => {
     return ( 
         <div className="welcome">
 <<<<<<< HEAD
+<<<<<<< HEAD
             <div className="padder"></div>
             <div className="container-fluid text-left welcome__title-box">
                 <h2 className="welcome__title font">Welcome.</h2>
                 <p className="welcome__description font"> Select a menu below</p>
+=======
+            <div className="container-fluid text-left welcome__title-box pl-0">
+                <h2 className="welcome__title font mt-2">Welcome.</h2>
+                <p className="welcome__description"> Please select a menu below</p>
+>>>>>>> parent of 745e6ca4... update
 =======
             <div className="container-fluid text-left welcome__title-box pl-0">
                 <h2 className="welcome__title font mt-2">Welcome.</h2>
@@ -81,9 +87,13 @@ const Welcome = () => {
             </div>
             <div className="container-fluid welcome__title-box-bottom mt-5 pr-0">
 <<<<<<< HEAD
+<<<<<<< HEAD
                 <h3 className="welcome__description-bottom-header font">All recipes</h3>
                 <p className="welcome__description-bottom font">are sourced by Grandma from all over the net, editted and perfected by her expert touch.</p>
             </div>
+=======
+                <p className="welcome__description-bottom mb-0">All recipes are sourced by Grandma from all over the net, editted and perfected by her expert touch.</p>
+>>>>>>> parent of 745e6ca4... update
 =======
                 <p className="welcome__description-bottom mb-0">All recipes are sourced by Grandma from all over the net, editted and perfected by her expert touch.</p>
 >>>>>>> parent of 745e6ca4... update
