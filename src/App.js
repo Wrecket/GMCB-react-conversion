a3137937be1abe3d1018abfe32c4d8757ee0741a
Matthew Fuller <matt.j.fuller@outlook.com>
2020-10-15 14:31:31 +0100

    update

--- src/App.js
+++ src/App.js
@@ -5,18 +5,17 @@ import RecipeList from './Component/RecipeList/RecipeList';
 import { Switch, Route, BrowserRouter } from 'react-router-dom'
 import RecipeItemDetails from './Component/RecipeItemDetails/RecipeItemDetails';
 import Welcome from './Component/Welcome/Welcome';
-import Footer from './Component/Footer/Footer';
 
 function App() {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Switch>
-        <div className="App">
+        <React.Fragment>
           <Navbar/>
           <Route exact path="/" component={Welcome} />
           <Route exact path="/recipes" component={RecipeList} />
           <Route path="/recipes/:id" component={RecipeItemDetails} />
-        </div>
+          </React.Fragment>
       </Switch>
     </BrowserRouter>
   );
--- src/Component/Welcome/Welcome.js
+++ src/Component/Welcome/Welcome.js
@@ -2,27 +2,71 @@ import React from 'react';
 import './Welcome.css'
 import Filteritem from '../FilterItem/FilterItem';
 import recipes from '../data/recipes.json'
+import {Link} from 'react-router-dom'
 
 
 const Nrec = () => {
     let newRecipe = recipes.filter(recipe => {
         return recipe.id === (recipes.length) 
     }) 
-    console.log(newRecipe[0])
     return (
-        <div className="welcome__new-description-new-rec container-fluid">
-                <div className=" welcome__new-menu-box-outer">
-                        <div className="welcome__new-menu-box-inner">
-                            <img src={newRecipe[0].image} alt="mains" className="welcome__new-menu-box-img"/>
-                            <div className="welcome__new-menu-name-wrap">
-                                <h3 className="welcome__new-menu-box-img-text2 font">Newest Recipe:</h3>
-                                <h3 className="welcome__new-menu-box-img-text font">{newRecipe[0].name}</h3>
-                            </div>
+        
+            <div className=" welcome__menu-box-outer newrec">
+                    <div className="welcome__menu-box-inner">
+                    <h2 className="welcome__new-recipe font">Newest Recipe</h2>
+                    <Link to={`/recipes/${recipes.length}`} id={newRecipe.name} className="recipe__link">
+                        <img src={newRecipe[0].image} alt={newRecipe[0].name} className="welcome__menu-box-img"/>
+                        <div className="welcome__menu-name-wrap">
+                            <h3 className="welcome__menu-box-img-text font">{newRecipe[0].name}</h3>
                         </div>
-                </div>
-            </div>
+                    </Link>    
+                    </div>
+             </div>
+        
       );
     }
+
+    const Rrec = () => {
+        let random = Math.floor(Math.random() * (Math.floor(1+(recipes.length))))
+        let newRecipe = recipes.filter(recipe => {
+            return recipe.id === random
+        }) 
+        return (
+                <div className=" welcome__menu-box-outer newrec">
+                        <div className="welcome__menu-box-inner">
+                        <h2 className="welcome__new-recipe font">Random Recipe</h2>
+                        <Link to={`/recipes/${random}`} id={newRecipe.name} className="recipe__link">
+                            <img src={newRecipe[0].image} alt={newRecipe[0].name} className="welcome__menu-box-img"/>
+                            <div className="welcome__menu-name-wrap">
+                                <h3 className="welcome__menu-box-img-text font">{newRecipe[0].name}</h3>
+                            </div>
+                        </Link>    
+                        </div>
+                 </div>
+          );
+        }
+        const Frec = () => {
+            let newRecipe = recipes.filter(recipe => {
+                return recipe.id === 8
+            }) 
+            return (
+                
+                    <div className=" welcome__menu-box-outer newrec">
+                            <div className="welcome__menu-box-inner">
+                            <h2 className="welcome__new-recipe font">Featured Recipe</h2>
+                            <Link to={`/recipes/${recipes.length}`} id={newRecipe.name} className="recipe__link">
+                                <img src={newRecipe[0].image} alt={newRecipe[0].name} className="welcome__menu-box-img"/>
+                                <div className="welcome__menu-name-wrap">
+                                    <h3 className="welcome__menu-box-img-text font">{newRecipe[0].name}</h3>
+                                </div>
+                            </Link>    
+                            </div>
+                     </div>
+                
+              );
+            }
+        
+    
 
 const Welcome = () => {
 
@@ -60,66 +104,21 @@ const Welcome = () => {
     
     return ( 
         <div className="welcome">
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-            <div className="padder"></div>
-            <div className="container-fluid text-left welcome__title-box">
-                <h2 className="welcome__title font">Welcome.</h2>
-                <p className="welcome__description font"> Select a menu below</p>
-=======
-            <div className="container-fluid text-left welcome__title-box pl-0">
-                <h2 className="welcome__title font mt-2">Welcome.</h2>
-                <p className="welcome__description"> Please select a menu below</p>
->>>>>>> parent of 745e6ca4... update
-=======
-            <div className="container-fluid text-left welcome__title-box pl-0">
-                <h2 className="welcome__title font mt-2">Welcome.</h2>
-                <p className="welcome__description"> Please select a menu below</p>
->>>>>>> parent of 745e6ca4... update
-=======
-            <div className="container-fluid text-left welcome__title-box pl-0">
-                <h2 className="welcome__title font mt-2">Welcome.</h2>
-                <p className="welcome__description"> Please select a menu below</p>
->>>>>>> parent of 745e6ca4... update
-=======
-            <div className="container-fluid text-left welcome__title-box pl-0">
-                <h2 className="welcome__title font mt-2">Welcome.</h2>
-                <p className="welcome__description"> Please select a menu below</p>
->>>>>>> parent of 745e6ca4... update
+            <div className="container-fluid">
+                <div className=" title-wrap container-fluid text-left">
+                    <div className="welcome__title-wrap font container-fluid">
+                        <h1 className="">Welcome.</h1>
+                        <p>Grandma's Cookbook is a personal online repository, made to provide those that seek good food with the recipes needed to create it. Each recipe within this site has been sourced from all over the internet, edited and doctored by Grandma's magic touch, perfected and presented ready for use.</p>
+                        <p className="">Please select a menu below</p>
+                    </div>
+                </div>    
+                <div className="welcome__menu-box container-fluid">
+                    {filters.map((filter) => <Filteritem key={filter.id} filter={filter} />)}
+                    {Nrec()}
+                    {Rrec()}
+                    {Frec()}
+                </div>
             </div>
-            <div className="welcome__menu-box">
-                {filters.map((filter) => <Filteritem key={filter.id} filter={filter} />)}
-            </div>
-            <div className="padder"></div>
-            <div className="d-flex">
-            <div className="welcome__new-rec">
-                {Nrec()}
-            </div>
-            <div className="container-fluid welcome__title-box-bottom mt-5 pr-0">
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-                <h3 className="welcome__description-bottom-header font">All recipes</h3>
-                <p className="welcome__description-bottom font">are sourced by Grandma from all over the net, editted and perfected by her expert touch.</p>
-            </div>
-=======
-                <p className="welcome__description-bottom mb-0">All recipes are sourced by Grandma from all over the net, editted and perfected by her expert touch.</p>
->>>>>>> parent of 745e6ca4... update
-=======
-                <p className="welcome__description-bottom mb-0">All recipes are sourced by Grandma from all over the net, editted and perfected by her expert touch.</p>
->>>>>>> parent of 745e6ca4... update
-=======
-                <p className="welcome__description-bottom mb-0">All recipes are sourced by Grandma from all over the net, editted and perfected by her expert touch.</p>
->>>>>>> parent of 745e6ca4... update
-=======
-                <p className="welcome__description-bottom mb-0">All recipes are sourced by Grandma from all over the net, editted and perfected by her expert touch.</p>
->>>>>>> parent of 745e6ca4... update
-            </div>
-            
-            
         </div>
      );
 }
